cc4921efee0a47032c22c2395f46156fc8e3e353
Gaurav Singla <76846838+GauravSingla-1@users.noreply.github.com>
2021-07-03 20:18:47 +0530

    Delete Money-Manager directory

--- Money-Manager/money manager create database.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-a=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='1234'
-    )
-mycursor=a.cursor()
-mycursor.execute('create database banking')
